9e35d75a0b752395e982cecc4ef4dd70c8900582
hexpell <hexpell@tuta.io>
2022-08-07 00:00:00 +0000

    Do not trigger memory read if MOVQ dst operand is a memory access

--- src/libtriton/arch/x86/x86Semantics.cpp
+++ src/libtriton/arch/x86/x86Semantics.cpp
@@ -9266,30 +9266,29 @@ namespace triton {
         auto& dst = inst.operands[0];
         auto& src = inst.operands[1];
 
-        auto op1 = this->symbolicEngine->getOperandAst(inst, dst);
-        auto op2 = this->symbolicEngine->getOperandAst(inst, src);
+        auto op1 = this->symbolicEngine->getOperandAst(inst, src);
 
         /* Create the semantics */
         triton::ast::SharedAbstractNode node = nullptr;
 
         /* when operating on MMX technology registers and memory locations */
         if (dst.getBitSize() == triton::bitsize::qword && src.getBitSize() == triton::bitsize::qword)
-          node = op2;
+          node = op1;
 
         /* when source and destination operands are XMM registers */
         else if (dst.getBitSize() == triton::bitsize::dqword && src.getBitSize() == triton::bitsize::dqword)
           node = this->astCtxt->concat(
-                  this->astCtxt->extract(triton::bitsize::dqword-1, triton::bitsize::qword, op1),
-                  this->astCtxt->extract(triton::bitsize::qword-1, 0, op2)
+                  this->astCtxt->extract(triton::bitsize::dqword-1, triton::bitsize::qword, this->symbolicEngine->getOperandAst(inst, dst)),
+                  this->astCtxt->extract(triton::bitsize::qword-1, 0, op1)
                  );
 
         /* when source operand is XMM register and destination operand is memory location */
         else if (dst.getBitSize() < src.getBitSize())
-          node = this->astCtxt->extract(triton::bitsize::qword-1, 0, op2);
+          node = this->astCtxt->extract(triton::bitsize::qword-1, 0, op1);
 
         /* when source operand is memory location and destination operand is XMM register */
         else if (dst.getBitSize() > src.getBitSize())
-          node = this->astCtxt->zx(triton::bitsize::qword, op2);
+          node = this->astCtxt->zx(triton::bitsize::qword, op1);
 
         /* Invalid operation */
         else
